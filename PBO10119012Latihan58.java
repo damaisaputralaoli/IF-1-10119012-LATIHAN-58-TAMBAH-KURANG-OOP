ae7b5dba0be501b7f9db94ac04bf4dadcc0dd081
damaisaputralaoli <73173498+damaisaputralaoli@users.noreply.github.com>
2020-11-22 11:19:40 +0700

    Add files via upload

---
+++ PBO10119012Latihan58.java
@@ -0,0 +1,20 @@
+
+package pbo10119012latihan58;
+
+/**
+ *  NAMA   : DAMAI SAPUTRA LAOLI
+ *  KELAS  : IF-1
+ *  NIM    : 10119012
+ * @author damai
+ */
+public class PBO10119012Latihan58 {
+
+    
+    public static void main(String[] args) {
+        JumlahBilangan bilangan1 = new JumlahBilangan();
+        bilangan1.tampilHasilJumlah();
+        SelisihBilangan oo2 = new SelisihBilangan();
+        oo2.tampilSelisih();
+    }
+    
+}
